1f00f6c855bfdd23540e64644bf09d0851ddbe62
kendell <KTibow@users.noreply.github.com>
2020-03-26 14:41:06 -0700

    Update google_logout.ino

--- google_logout.ino
+++ google_logout.ino
@@ -1,16 +1,45 @@
+// From startup it takes 2.4 seconds to run
+
 void setup() {
   // initialize control over the keyboard:
   Keyboard.begin();
   // initialize the led as out
   pinMode(13, OUTPUT);
-  delay(1100);
+  // blink to say waiting for machine
+  digitalWrite(13, HIGH);
+  delay(275);
+  digitalWrite(13, LOW);
+  delay(275);
+  digitalWrite(13, HIGH);
+  delay(275);
+  digitalWrite(13, LOW);
+  delay(275);
+  // Open linux run dialog
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(KEY_F2);
+  Keyboard.releaseAll();
+  delay(200);
+  // Open spotlight
+  // Press 3 times because on Windows this changes keybaord layout
+  for (int i = 0; i < 3; i++){
+    Keyboard.press(KEY_LEFT_GUI);
+    Keyboard.press(' ');
+    Keyboard.releaseAll();
+    delay(200);
+  }
   // Open run
   Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press('r');
   Keyboard.releaseAll();
-  delay(300);
-  // Open webpage
+  delay(200);
+  // If on some other thing, delete characters (there could be "   r")
+  Keyboard.write(KEY_BACKSPACE);
+  Keyboard.write(KEY_BACKSPACE);
+  Keyboard.write(KEY_BACKSPACE);
+  Keyboard.write(KEY_BACKSPACE);
+  // Open webpage. Comment out the google-logout url and uncomment the example.com url to make it not log you out.
   Keyboard.print("https://ktibow.github.io/google-logout/");
+  //Keyboard.print("http://example.com/");
   delay(200);
   Keyboard.write(KEY_RETURN);
   // Blink the LED to indicate done
