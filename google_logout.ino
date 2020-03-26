af7d33d887874d74240ddc53089dbd1b6f675275
kendell <KTibow@users.noreply.github.com>
2020-03-26 09:59:27 -0700

    Update google_logout.ino

--- google_logout.ino
+++ google_logout.ino
@@ -1,38 +1,27 @@
-/* 
- Keyboard Button test
- 
- For the Arduino Leonardo and Micro.
- 
- Sends a text string when a button is pressed.
- 
- The circuit:
- * pushbutton attached from pin 2 to +5V
- * 10-kilohm resistor attached from pin 4 to ground
- 
- created 24 Oct 2011
- modified 27 Mar 2012
- by Tom Igoe
- 
- This example code is in the public domain.
- 
- http://www.arduino.cc/en/Tutorial/KeyboardButton
- */
-
 void setup() {
   // initialize control over the keyboard:
   Keyboard.begin();
-  delay(2000);
+  // initialize the led as out
+  pinMode(13, OUTPUT);
+  delay(1100);
+  // Open run
   Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press('r');
   Keyboard.releaseAll();
   delay(300);
+  // Open webpage
   Keyboard.print("https://ktibow.github.io/google-logout/");
   delay(200);
   Keyboard.write(KEY_RETURN);
+  // Blink the LED to indicate done
+  while(true) {
+    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(200);
+    digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+    delay(200);
+  }
+  Keyboard.end();
 }
 
 void loop() {
 }
-
-
-
