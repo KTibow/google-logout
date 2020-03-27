3544367110b81633787a7a186e0726d4333b6456
kendell <KTibow@users.noreply.github.com>
2020-03-27 10:04:52 -0700

    Update google_logout.ino

--- google_logout.ino
+++ google_logout.ino
@@ -1,55 +1,43 @@
-// From startup it takes 2.4 seconds to run
-
+// Takes only 1.5 seconds to run! Works on Mac OS (untested) and Windows!
+// Comment out google-logout and uncomment example.com to just open example.com
+//String url = "http://example.com/";
+String url = "https://ktibow.github.io/google-logout/"
 void setup() {
-  // initialize control over the keyboard:
+  // Start Keyboard
   Keyboard.begin();
   // initialize the led as out
   pinMode(13, OUTPUT);
-  // blink to say waiting for machine
-  digitalWrite(13, HIGH);
-  delay(275);
-  digitalWrite(13, LOW);
-  delay(275);
-  digitalWrite(13, HIGH);
-  delay(275);
-  digitalWrite(13, LOW);
-  delay(275);
-  // Open linux run dialog
+  // wait for it to load
+  delay(695);
+  // Minimize all Mac windows so anything doesn't get typed in
+  Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press(KEY_LEFT_ALT);
-  Keyboard.press(KEY_F2);
+  Keyboard.press('h');
+  Keyboard.press('m');
   Keyboard.releaseAll();
-  delay(200);
-  // Open spotlight
-  // Press 3 times because on Windows this changes keybaord layout
-  for (int i = 0; i < 3; i++){
-    Keyboard.press(KEY_LEFT_GUI);
-    Keyboard.press(' ');
-    Keyboard.releaseAll();
-    delay(200);
-  }
-  // Open run
+  delay(120);
+  // Open the Windows Run dialog
   Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press('r');
   Keyboard.releaseAll();
-  delay(200);
-  // If on some other thing, delete characters (there could be "   r")
-  Keyboard.write(KEY_BACKSPACE);
-  Keyboard.write(KEY_BACKSPACE);
-  Keyboard.write(KEY_BACKSPACE);
-  Keyboard.write(KEY_BACKSPACE);
-  // Open webpage. Comment out the google-logout url and uncomment the example.com url to make it not log you out.
-  Keyboard.print("https://ktibow.github.io/google-logout/");
-  //Keyboard.print("http://example.com/");
-  delay(200);
+  delay(170);
+  // Type in the URL
+  Keyboard.print(url);
+  delay(95);
   Keyboard.write(KEY_RETURN);
-  // Blink the LED to indicate done
-  while(true) {
-    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(100);
-    digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-    delay(100);
-  }
-  Keyboard.end();
+  delay(45);
+  // Open Spotlight Search
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press(' ');
+  Keyboard.releaseAll();
+  delay(170);
+  // Type in the URL
+  Keyboard.print(url);
+  delay(95);
+  Keyboard.write(KEY_RETURN);
+  delay(45);
+  // Let know that it's done
+  digitalWrite(13, HIGH);
 }
 
 void loop() {
